d5a005e731eb840277a9aa3ae180de3ee188fd64
pablo <pablo.makoki@gmail.com>
2018-12-13 23:14:04 +0100

    apimodelo

---
+++ lib/api/apiModelo.dart
@@ -0,0 +1,12 @@
+class ApiModelo {
+  static generaRegistroDispatcher() {}
+  static generaRegistroJugador() {}
+  static generaRegistroImperio() {}
+  static generaRegistroProvincia() {}
+  static generaRegistroCapital() {}
+  static generaRegistroPalacio() {}
+  static generaRegistroSilos() {}
+  static generaRegistroCuartel() {}
+  static generaRegistroCentroDeInvestigacion() {}
+
+}
